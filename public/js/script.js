25261c02789db5adce840f5639cff99438294240
Diego Zuccolilli <Dzuccolilli@gmail.com>
2025-03-14 19:25:56 -0300

    modificaciones a los archivos de la pagina de  login y principal

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.getElementById('button-login');
     const signupButton = document.getElementById('button-signup');
+    const homeButton = document.getElementById('button-home');
+    const logoutButton = document.getElementById('button-logout');
+
 
     loginButton.addEventListener('click', function() {
         location.href = 'login.html';
@@ -9,4 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
     signupButton.addEventListener('click', function() {
         location.href = 'signup.html';
     });
+
+    homeButton.addEventListener('click', function() {
+        location.href = 'pagina-principal.html';
+    });
+
 });
